b2d93d77c5a6b7a880f1d95d97aafb94a48d0d57
ohsangwon <axur123@gmail.com>
2022-07-09 01:49:20 +0900

    #터치 작동 범위 제한.

---
+++ Assets/0.Project/Script/Debug/TrasformReturner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrasformReturner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(name + " : " + transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/0.Project/Script/TouchFollowScript.cs
+++ Assets/0.Project/Script/TouchFollowScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class TouchFollowScript : MonoBehaviour
 {
-    
+
     [SerializeField]
     Vector3 _touchPosition;
     [SerializeField]
@@ -11,13 +11,23 @@ public class TouchFollowScript : MonoBehaviour
     Vector3 _direction;
     [SerializeField]
     float moveSpeed = 10f;
-    [SerializeField]
 
     // Start is called before the first frame update
+
+
+    [SerializeField]
+    Transform _moveLU, _moveRD, _touchLU;
+
+    Vector2 _moveLUvec, _moveRDvec, _touchLUvec;
     void Awake()
     {
         _thisRB = GetComponent<Rigidbody2D>();
+        _moveLUvec = _moveLU.position;
+        _moveRDvec = _moveRD.position;
+        _touchLUvec = _touchLU.position;
     }
+
+
 
 
 
@@ -29,28 +39,46 @@ public class TouchFollowScript : MonoBehaviour
         //절반아래일때만
         LineTracer.instance.SetDrawLineStart(transform.position);
 
-        if (Input.touchCount > 0){
+        if (Input.touchCount > 0)
+        {
             Touch _touch = Input.GetTouch(0);
             _touchPosition = Camera.main.ScreenToWorldPoint(_touch.position);
-            _touchPosition.z = 0;
-            _direction = _touchPosition - transform.position;
-            _direction = _direction.normalized;
-            _thisRB.velocity = new Vector2(_direction.x, _direction.y) * moveSpeed;
+            SetPlayerPos(_touchPosition);
+
             if (_touch.phase == TouchPhase.Ended)
                 _thisRB.velocity = Vector2.zero;
-
-            
-
         }
-        else {
-        
+        else
+        {
+
         }
 
 
 
     }
 
+    //터치 상한선과 작동 상한선 두가지로 나누자.
+    void SetPlayerPos(Vector3 _targetPos)
+    {
+        //먼저 현재 캐릭터가 제한 범위를 넘어갈경우 잡아주는걸 해야한다.
+        
+        if (_touchLUvec.x < _targetPos.x && _moveRDvec.x > _targetPos.x &&
+        _touchLUvec.y > _targetPos.y && _moveRDvec.y < _targetPos.y)
+        {
+            _targetPos.z = 0;
+            _direction = _targetPos - transform.position;
+            _direction = _direction.normalized;
+            _thisRB.velocity = new Vector2(_direction.x, _direction.y) * moveSpeed;
+        }
+        else
+            _thisRB.velocity = Vector2.zero;
 
-  
+        Vector3 _curTF = transform.position;
+        _curTF.x = Mathf.Clamp(_curTF.x, _moveLUvec.x, _moveRDvec.x);
+        _curTF.y = Mathf.Clamp(_curTF.y, _moveRDvec.y, _moveLUvec.y);
+        transform.position = _curTF;
+    }
+
+
 
 }
