ac0f64fdbce65a53757f3024c04eb697fc3c3898
sanon531 <axur123@gmail.com>
2022-07-21 21:20:26 +0900

    # 배율 관련 버그 수정

--- Assets/0.Project/Script/LineTracer.cs
+++ Assets/0.Project/Script/LineTracer.cs
@@ -19,8 +19,8 @@ public class LineTracer : MonoBehaviour
 
     private void Awake(){instance = this;}
 
+
     //여기 내부에서 현재 배율에대한정보를 그냥 패턴 매니져에서 담당함. 
-    // 그리고 여기서ㅡㄴ 위치에 대한 정보만 담당하여서 배율을 처리하지않음으로 효율성을 늘린다
     public void SetDrawLineStart(Vector2 _startPosition)
     {
         _linerenderer.SetPosition(1, _startPosition);
@@ -28,6 +28,8 @@ public class LineTracer : MonoBehaviour
         _textForShowRatio.transform.position = _lastLineEndPos + (_lastLineStartPos - _lastLineEndPos) /2 ;
         _textForShowRatio.SetText(Math.Round(GetLengthRatio(), 1).ToString());
     }
+
+
     public void SetDrawLineEnd(Vector2 _endPosition)
     {
         _linerenderer.SetPosition(0, _endPosition);
@@ -36,8 +38,8 @@ public class LineTracer : MonoBehaviour
 
     float GetLengthRatio()
     {
-        float _xval = Mathf.Pow(_lastLineStartPos.x - _lastLineEndPos.x, 2);
-        float _yval = Mathf.Pow(_lastLineStartPos.y - _lastLineEndPos.y, 2);
+        float _xval = Mathf.Pow((_lastLineStartPos.x - _lastLineEndPos.x) / 1.75f, 2);
+        float _yval = Mathf.Pow((_lastLineStartPos.y - _lastLineEndPos.y) / 1.75f, 2);
 
         return Mathf.Sqrt(_xval + _yval);
     }
