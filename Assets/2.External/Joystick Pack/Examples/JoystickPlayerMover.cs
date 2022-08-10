0a9f97a7550fb8362996de7b3e9bad47c7525078
sanon531 <axur123@gmail.com>
2022-08-10 22:50:12 +0900

    # 간단한 조이스틱 구현

---
+++ Assets/2.External/Joystick Pack/Examples/JoystickPlayerMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace PG.Battle
+{
+    public class JoystickPlayerMover : MonoBehaviour
+    {
+        public float speed;
+        public FixedJoystick variableJoystick;
+        [SerializeField]
+        Transform _moveLU, _moveRD;
+        Vector2 _moveLUvec, _moveRDvec;
+        [SerializeField]
+        Rigidbody2D _thisRB;
+
+        private void Awake()
+        {
+            _moveLUvec = _moveLU.position;
+            _moveRDvec = _moveRD.position;
+
+        }
+        public void FixedUpdate()
+        {
+            LineTracer.instance.SetDrawLineStart(transform.position);
+
+            Vector3 _direction = Vector3.up * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+            _direction = _direction.normalized;
+            _thisRB.velocity = _direction * speed;
+            Vector3 _curTF = transform.position;
+            _curTF.x = Mathf.Clamp(_curTF.x, _moveLUvec.x, _moveRDvec.x);
+            _curTF.y = Mathf.Clamp(_curTF.y, _moveRDvec.y, _moveLUvec.y);
+            transform.position = _curTF;
+        }
+    }
+
+}
