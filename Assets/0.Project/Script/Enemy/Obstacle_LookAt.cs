bb62b54b5cc1f39058bfce2b7b6bd8e98b76fff8
Hyein <ms8370@naver.com>
2022-08-16 23:40:25 +0900

    0816 플레이어 방향으로 날아가는 장애물 추가
    임시 sprite "Arrow.png"도 함께 추가

---
+++ Assets/0.Project/Script/Enemy/Obstacle_LookAt.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PG.Event;
+
+namespace PG.Battle
+{
+    public class Obstacle_LookAt : Obstacle
+    {
+        private Transform playerTr;
+        private Vector2 forward = new Vector2(0, -1);   //sprite의 방향에 따라 달라짐
+
+        [SerializeField]
+        private float rotateSpeed;
+
+        [SerializeField]
+        private float moveSpeed;
+
+
+        private void Start()
+        {
+            Global_BattleEventSystem._onNonTotalPause += SetNonTotalPauseOn;
+            Global_BattleEventSystem._offNonTotalPause += SetNonTotalPauseOff;
+            playerTr = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        private void OnDestroy()
+        {
+            Global_BattleEventSystem._onNonTotalPause -= SetNonTotalPauseOn;
+            Global_BattleEventSystem._offNonTotalPause -= SetNonTotalPauseOff;
+        }
+        void Update()
+        {
+            CheckStatus();
+            LookPlayer();
+            Shoot();
+        }
+
+        private void LookPlayer()
+        {
+            if (_isActive) return;
+
+            Vector2 direction = new Vector2(
+                     transform.position.x - playerTr.position.x,
+                     transform.position.y - playerTr.position.y
+                 );
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion angleAxis = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+            Quaternion rotation = Quaternion.Slerp(transform.rotation, angleAxis, rotateSpeed * Time.deltaTime);
+            transform.rotation = rotation;
+        }
+
+        private void Shoot()
+        {
+            if (!_isActive) return;
+
+            transform.Translate(forward * moveSpeed * Time.deltaTime);
+        }
+    }
+}
