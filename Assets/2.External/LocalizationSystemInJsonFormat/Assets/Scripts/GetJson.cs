940fde36c83e7a9c72be971d04c352b37e574887
sanon531 <axur123@gmail.com>
2022-08-28 22:54:49 +0900

    # 로컬라이제이션용 툴 삽입.

---
+++ Assets/2.External/LocalizationSystemInJsonFormat/Assets/Scripts/GetJson.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class GetJson : MonoBehaviour
+{
+    [SerializeField] private string _nameFile;
+
+    [HideInInspector] public List<JsonStructure> Items = new List<JsonStructure>();
+
+    private string _lang;
+
+    private void Awake()
+    {
+        LanguageDefinition();
+
+        LoadItem(getPath(_nameFile, _lang));
+    }
+
+    //system language detection or stored in the registry
+    private void LanguageDefinition() {
+        Debug.Log("System Language - " + Application.systemLanguage);
+
+        if (!PlayerPrefs.HasKey("lang"))
+        {
+            if (Application.systemLanguage == SystemLanguage.Russian)
+                PlayerPrefs.SetString("lang", "ru");
+            else
+                PlayerPrefs.SetString("lang", "en");
+        }
+
+        _lang = PlayerPrefs.GetString("lang");
+    }
+
+    //updating the values ​​of text fields when changing the language
+    public void ReloadLang()
+    {
+        _lang = PlayerPrefs.GetString("lang");
+        LoadItem(getPath(_nameFile, _lang));
+        SetValueLang[] scr = FindObjectsOfType<SetValueLang>();
+
+        foreach (SetValueLang item in scr)
+            item.SetValueText();
+    }
+
+    //Loading values ​​from json file by keys
+    public void LoadItem(string path)
+    {
+        Debug.Log(path);
+        if (File.Exists(path))
+        {
+            string jsonString = File.ReadAllText(path, Encoding.UTF8);
+            JsonStructure[] myItem = JsonHelper.FromJson<JsonStructure>(jsonString);
+            Items.Clear();
+
+            foreach (JsonStructure item in myItem)
+                Items.Add(item);
+        }
+        else
+        {
+            Debug.LogError("Json file not found " + "\"" + _lang + "/" + _nameFile + ".json\"");
+        }
+
+    }
+
+    //Path where json files are located
+    private string getPath(string name, string lang)
+    {
+#if UNITY_EDITOR
+        return Application.dataPath + "/2.External/StreamingAssets/Languages/" + lang + "/" + name + ".json";
+#elif UNITY_ANDROID
+		return Application.persistentDataPath+"/2.External/StreamingAssets/Languages/" + lang + "/" + name + ".json";
+#elif UNITY_IPHONE
+		return Application.persistentDataPath+"/2.External/StreamingAssets/Languages/" + lang + "/" + name + ".json";
+#else
+        return Application.dataPath + "/2.External/StreamingAssets/Languages/" + lang + "/" + name + ".json";
+#endif
+    }
+
+}
+
+[Serializable]
+public class JsonStructure
+{
+    public string key;
+    public string value;
+}
+
---
+++ Assets/2.External/LocalizationSystemInJsonFormat/Assets/Scripts/GetLanguages.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class GetLanguages : MonoBehaviour
+{
+    [SerializeField] private Transform parentSpawnLangButt;
+
+    [SerializeField] private GameObject langButt;
+
+    void Start()
+    {
+        var path = getPath();
+        var files = Directory.GetDirectories(path);
+        foreach (string fileName in files)
+        {
+            SpawnLangButtons(fileName + "/config.json");
+        }
+    }
+
+    void SpawnLangButtons(string path)
+    {
+        if (File.Exists(path))
+        {
+            string jsonString = File.ReadAllText(path, Encoding.UTF8);
+            JsonStructureConfig[] myItem = JsonHelper.FromJson<JsonStructureConfig>(jsonString);
+
+            foreach (JsonStructureConfig item in myItem)
+            {
+                GameObject butt;
+                if (item.key == "config")
+                {
+                    butt = Instantiate(langButt, parentSpawnLangButt.position, Quaternion.identity) as GameObject;
+                    butt.transform.SetParent(parentSpawnLangButt);
+                    butt.GetComponent<ConfigButtLang>().title = item.title;
+                    butt.GetComponent<ConfigButtLang>().key_lang = item.key_lang;
+                    butt.transform.localScale = new Vector3(1f, 1f, 1f);
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("Json file config not found " + "\"" + path + "\"");
+        }
+
+    }
+
+    private string getPath()
+    {
+#if UNITY_EDITOR
+        return Application.dataPath + "/2.External/StreamingAssets/Languages/";
+#elif UNITY_ANDROID
+		return Application.persistentDataPath+"/2.External/StreamingAssets/Languages/";
+#elif UNITY_IPHONE
+		return Application.persistentDataPath+"/2.External/StreamingAssets/Languages/";
+#else
+        return Application.dataPath + "/2.External/StreamingAssets/Languages/";
+#endif
+    }
+
+}
+
+[Serializable]
+public class JsonStructureConfig
+{
+    public string key;
+    public string title;
+    public string key_lang;
+}
