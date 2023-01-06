b3cd5b5ae22f843fb2aa15a057466303114dc278
Hyein <ms8370@naver.com>
2023-01-07 04:36:23 +0900

    옵션창 작업
    에셋 패키지 하나 다운 받아서 임시로 작업하였음
    돌아가기, 메인으로 가기, 배경음/효과음 볼륨조절 + 음소거 기능

--- Assets/0.Project/Script/Manager/AudioManager.cs
+++ Assets/0.Project/Script/Manager/AudioManager.cs
@@ -11,11 +11,55 @@ namespace PG
         AudioSource _backgroundmusic;
         [SerializeField]
         List<AudioSource> _audioList = new List<AudioSource>();
+        [SerializeField]
+        List<AudioSource> _effectSoundList = new List<AudioSource>();
 
         // Start is called before the first frame update
         void Start()
         {
 
+        }
+
+        public float GetBackgroundVolume()
+        {
+            return _backgroundmusic.volume;
+        }
+
+        public float GetEffectVolume()
+        {
+            if(_effectSoundList.Count != 0)
+            {
+                return _effectSoundList[0].volume;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void ChangeBackgroundVolume(float value) 
+        {
+            _backgroundmusic.volume = value;
+        }
+
+        public void ChangeEffectVolume(float value)
+        {
+            foreach(AudioSource source in _effectSoundList) 
+            {
+                source.volume = value;
+            }
+        }
+
+        public void MuteBackgroundVolume(bool value)
+        {
+            _backgroundmusic.mute = value;
+        }
+        public void MuteEffectVolume(bool value)
+        {
+            foreach (AudioSource source in _effectSoundList)
+            {
+                source.mute = value;
+            }
         }
 
         public static void CallSFX(string _string)
